89cac5ebf35cd52e7dd4b2860ca2afd0e271c880
pottj <pott.ne.ja@gmail.com>
2022-03-11 15:44:05 +0100

    update script 04

--- scripts/04_LeadSNPs_GTExv8Comparison.R
+++ scripts/04_LeadSNPs_GTExv8Comparison.R
@@ -19,8 +19,6 @@
 #' 
 #' In the first step, I load and filter the GTEx v8 data by tissue. I also save the matched data set, as I need this one for the co-localization analyses later. 
 #'  
-#' I assume the GTEx data is named *GTEx_Analysis_v8_QTLs_GTEx_Analysis_v8_eQTL_all_associations_TISSUE.allpairs.txt.gz*. If the files are named differently, please change code lines x & y accordingly. 
-#' 
 #' # Init ####
 #' ***
 rm(list = ls())
@@ -42,8 +40,13 @@ pQTLs[,SNPGene:=paste(variant_id_hg38,gene,sep=":")]
 
 eQTL_LIFE = fread("../exampleData/eQTLs_LIFE_hg19.txt")
 
-load("../results/03_leadSNPs.RData")
-leadpQTLs[,SNPGene:=paste(variant_id_hg38,gene,sep=":")]
+leadpQTLs<-pQTLs[hierarch_fdr5proz==T,.SD[pval==min(pval)],by=.(gene,setting)]
+leadpQTLs[,table(gene,setting)]
+dummy<-pQTLs[variant_id_hg19 == "rs78811301:43740523:G:A" & setting=="males",]
+leadpQTLs = leadpQTLs[!(gene =="TFF3" & setting =="males"),]
+leadpQTLs = rbind(leadpQTLs,dummy)
+leadpQTLs[,table(setting)]
+setcolorder(leadpQTLs,names(pQTLs_a))
 leadpQTLs[,SNPGeneSetting:=paste(variant_id_hg38,gene,setting,sep=":")]
 leadpQTLs_a = leadpQTLs[setting == "combined"]
 leadpQTLs_m = leadpQTLs[setting == "males"]
@@ -51,85 +54,89 @@ leadpQTLs_f = leadpQTLs[setting == "females"]
 
 #' # Get GTEx eQTL data ####
 #' ***
+#' Please note: I commented out some lines of code as the interim results were saved in the *../temp/GTEx_v8_filtered directory.
+#' 
+#' In addition, I assume the GTEx data is named *GTEx_Analysis_v8_QTLs_GTEx_Analysis_v8_eQTL_all_associations_TISSUE.allpairs.txt.gz*. If the files are named differently, please change code lines 60 & 61 accordingly. 
+#' 
 myeQTLs<-dir(path = path_GTExv8,pattern = "GTEx_Analysis_v8_QTLs_GTEx_Analysis_v8")
-#myeQTLs = myeQTLs[c(1:6,49)]
 
 dumTab1<-foreach (i=c(1:length(myeQTLs)))%do%{
   #i=1
   myTissue<-gsub(".allpairs.txt.gz","",myeQTLs[i])
   myTissue<-gsub("GTEx_Analysis_v8_QTLs_GTEx_Analysis_v8_eQTL_all_associations_","",myTissue)
   time1 = Sys.time()
-  
-  # Loading data
-  message("Loading eQTL data ",myTissue," number ",i," of ",length(myeQTLs))
-  data0<-fread(paste0(basicpath, "/01_daten/2007_GTEx_v8/",myeQTLs[i]))
-  time2 = Sys.time()
-  message("          Finished loading eQTL data in ",round(difftime(time2, time1, tz,units = "min"),2)," minutes")
-  
-  # Change GeneID
-  data0[,ENSG:=gsub(gene_id, pattern = "\\..*", replacement = "")]
-  # data2<-copy(data1)
-  # data1 = copy(data2)
-  
-  # Filtering data
-  message("          Filtering eQTL data ",myTissue," number ",i," of ",length(myeQTLs))
-  filt<-is.element(data0$ENSG,myTab$ENSG)
-  data0<-data0[filt,]
-  time3 = Sys.time()
-  message("          Finished filtering eQTL data in ",round(difftime(time3, time2, tz,units = "min"),2)," minutes")
-  
-  # Harmonisation of column names
-  message("          Harmonizing column names for ",myTissue," number ",i," of ",length(myeQTLs))
-  dummy<-unlist(strsplit(data0$variant_id,"_"))
-  chr<-dummy[seq(1,length(dummy),by=5)]
-  chr<-gsub("chr","",chr)
-  chr<-as.numeric(chr)
-  pos<-as.numeric(dummy[seq(2,length(dummy),by=5)])
-  other_allele<-dummy[seq(3,length(dummy),by=5)]
-  effect_allele<-dummy[seq(4,length(dummy),by=5)]
-  
-  data0[,chr:=chr,]
-  data0[,pos:=pos,]
-  data0[,other_allele:=other_allele,]
-  data0[,effect_allele:=effect_allele,]
-  data0[,n_samples:=round(ma_count/maf/2,1)]
-  setnames(data0,"slope_se","se")
-  setnames(data0,"pval_nominal","pval")
-  setnames(data0,"slope","beta")
-  
-  # Add gene name and cytoband
-  matched<-match(data0$ENSG,myTab$ENSG)
-  data0[,gene:=myTab[matched,Gene]]
-  data0[,cyto:=myTab[matched,Cytoband]]
-  data0[,tissue:=myTissue]
-  data0[,SNPGene:=paste(variant_id,gene,sep=":")]
-  
-  # Filtering: no duplicates, MAF>=0.01, SNPs in pQTLs
-  table(duplicated(data0$SNPGene),is.element(data0$variant_id,pQTLs$variant_id_hg38))
-  dups = data0[duplicated(data0$SNPGene),]
-  dups2 = data0[is.element(SNPGene,dups$SNPGene),]
-  dups3 = dups2[is.element(variant_id,pQTLs$variant_id_hg38),]
-  
-  data1 = rbind(data0[!is.element(SNPGene,dups$SNPGene),],dups3)
-  data1 = data1[maf>=0.01,]
-  data1 = data1[is.element(variant_id,pQTLs$variant_id_hg38),]
-  
-  table(is.element(pQTLs$variant_id_hg38,data1$variant_id))
-  table(is.element(data1$variant_id,pQTLs$variant_id_hg38))
-  
-  x1 = dim(data1)[1]
-  message("          Saving ",x1," filtered SNPs")
-  
-  # Save eqtl data
+
+  # # Loading data
+  # message("Loading eQTL data ",myTissue," number ",i," of ",length(myeQTLs))
+  # data0<-fread(paste0(basicpath, "/01_daten/2007_GTEx_v8/",myeQTLs[i]))
+  # time2 = Sys.time()
+  # message("          Finished loading eQTL data in ",round(difftime(time2, time1, tz,units = "min"),2)," minutes")
+  # 
+  # # Change GeneID
+  # data0[,ENSG:=gsub(gene_id, pattern = "\\..*", replacement = "")]
+  # # data2<-copy(data1)
+  # # data1 = copy(data2)
+  # 
+  # # Filtering data
+  # message("          Filtering eQTL data ",myTissue," number ",i," of ",length(myeQTLs))
+  # filt<-is.element(data0$ENSG,myTab$ENSG)
+  # data0<-data0[filt,]
+  # time3 = Sys.time()
+  # message("          Finished filtering eQTL data in ",round(difftime(time3, time2, tz,units = "min"),2)," minutes")
+  # 
+  # # Harmonisation of column names
+  # message("          Harmonizing column names for ",myTissue," number ",i," of ",length(myeQTLs))
+  # dummy<-unlist(strsplit(data0$variant_id,"_"))
+  # chr<-dummy[seq(1,length(dummy),by=5)]
+  # chr<-gsub("chr","",chr)
+  # chr<-as.numeric(chr)
+  # pos<-as.numeric(dummy[seq(2,length(dummy),by=5)])
+  # other_allele<-dummy[seq(3,length(dummy),by=5)]
+  # effect_allele<-dummy[seq(4,length(dummy),by=5)]
+  # 
+  # data0[,chr:=chr,]
+  # data0[,pos:=pos,]
+  # data0[,other_allele:=other_allele,]
+  # data0[,effect_allele:=effect_allele,]
+  # data0[,n_samples:=round(ma_count/maf/2,1)]
+  # setnames(data0,"slope_se","se")
+  # setnames(data0,"pval_nominal","pval")
+  # setnames(data0,"slope","beta")
+  # 
+  # # Add gene name and cytoband
+  # matched<-match(data0$ENSG,myTab$ENSG)
+  # data0[,gene:=myTab[matched,Gene]]
+  # data0[,cyto:=myTab[matched,Cytoband]]
+  # data0[,tissue:=myTissue]
+  # data0[,SNPGene:=paste(variant_id,gene,sep=":")]
+  # 
+  # # Filtering: no duplicates, MAF>=0.01, SNPs in pQTLs
+  # table(duplicated(data0$SNPGene),is.element(data0$variant_id,pQTLs$variant_id_hg38))
+  # dups = data0[duplicated(data0$SNPGene),]
+  # dups2 = data0[is.element(SNPGene,dups$SNPGene),]
+  # dups3 = dups2[is.element(variant_id,pQTLs$variant_id_hg38),]
+  # 
+  # data1 = rbind(data0[!is.element(SNPGene,dups$SNPGene),],dups3)
+  # data1 = data1[maf>=0.01,]
+  # data1 = data1[is.element(variant_id,pQTLs$variant_id_hg38),]
+  # 
+  # table(is.element(pQTLs$variant_id_hg38,data1$variant_id))
+  # table(is.element(data1$variant_id,pQTLs$variant_id_hg38))
+  # 
+  # x1 = dim(data1)[1]
+  # message("          Saving ",x1," filtered SNPs")
+  # 
+  # # Save eqtl data
   outfn1<-paste0("../temp/GTEx_v8_filtered/",myTissue,".RData")
-  save(data1,file=outfn1)
+  # save(data1,file=outfn1)
+  load(outfn1)
   
   # Check if lead SNP is in data
   check1<-table(is.element(myTab$ENSG, data1$ENSG))
   message("          Found ", check1[2], " of 92 candidate genes in eQTL data (matched)")
   
   # Get lead pQTLs
-  filt<-is.element(data1$SNPGene,leadSNPs$SNPGene)
+  filt<-is.element(data1$SNPGene,leadpQTLs$SNPGene)
   data2a<-data1[filt,]
   data2a[,comment := "matched_pQTL"]
   
@@ -362,6 +369,7 @@ leadpQTLs4[,LD_Dprime:=dumTab6[matched4,LD_Dprime]]
 leadpQTLs4[LeadIsBest==T,LD_r2:=1]
 leadpQTLs4[LeadIsBest==T,LD_Dprime:=1]
 head(leadpQTLs4)
+leadpQTLs4[,matchID := NULL]
 
 save(leadpQTLs4,file="../results/04_c_eQTL_lookup_Summary_LD.RData")
 
@@ -369,7 +377,7 @@ save(leadpQTLs4,file="../results/04_c_eQTL_lookup_Summary_LD.RData")
 #' ***
 leadpQTLs5 = copy(leadpQTLs4)
 leadpQTLs5 = leadpQTLs5[setting == "combined",]
-leadpQTLs5 = leadpQTLs5[tissue != "Whole_Blood_LIFE",]
+leadpQTLs5 = leadpQTLs5[tissue_GTEx != "Whole_Blood_LIFE",]
 
 z1<-leadpQTLs5[pval_GTEx<=0.05 & same_direction==F & !is.na(same_direction),.N,by=gene]
 z2<-leadpQTLs5[pval_GTEx<=0.05 & same_direction==T & !is.na(same_direction),.N,by=gene]
@@ -392,6 +400,7 @@ z3[ratio>=0.75,]
 #' Plotting Figure 2 ...
 myPlotData<-copy(leadpQTLs4)
 myPlotData<-myPlotData[setting =="combined",]
+myPlotData<-myPlotData[tissue_GTEx != "Whole_Blood_LIFE",]
 myPlotData<-myPlotData[pval_GTEx<=0.05,]
 length(unique(myPlotData$gene))
 table(is.element(myPlotData$gene,z3[ratio>=0.75,gene]))
@@ -436,7 +445,7 @@ myPlot2<-ggplot(myPlotData[flag2==T,], aes(x=beta_GTEx, y=beta, color=gene_color
                    box.padding = 1.15,
                    max.overlaps = 80,
                    show.legend=F,color="black")
-#myPlot2
+myPlot2
 
 tiff(filename = "../results/04_ScatterPlot_GEvsPE.tif", 
      width = 1200, height = 900, res=125, compression = 'lzw')

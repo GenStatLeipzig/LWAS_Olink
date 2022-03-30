2d2fdd8fe4fbf25a263036275a7c683ec6d02b00
pottj <pott.ne.ja@gmail.com>
2022-03-30 09:02:21 +0200

    add Olink gene information for LIFE Heart add first scripts for LIFE Heart

---
+++ scripts_Heart/02_Hierarchical_FDR.R
@@ -0,0 +1,144 @@
+#' ---
+#' title: "Hierarchical FDR"
+#' subtitle: "LWAS Olink - Heart"
+#' author: "Janne Pott"
+#' date: "29. March 2022"
+#' output:
+#'   html_document:
+#'     toc: true
+#'     number_sections: true
+#'     toc_float: true
+#'     code_folding: show
+#' ---
+#'
+#' # Intro ####
+#' ***
+#' I want to use hierarchical FDR for my Olink results. For this, I use the *addHierarchFDR()* function from the R-package *toolboxH* from Holger Kirsten (co-author of the publication; member of group Genstat at IMISE). You can download it from github via *devtools::install_github('holgerman/toolboxH')*
+#' 
+#' # Init ####
+#' ***
+rm(list = ls())
+time0<-Sys.time()
+
+source("../mySourceFile.R")
+.libPaths()
+setwd(projectpath_main_Heart)
+
+#' # Prep ####
+#' ***
+myTab<-fread("../temp_Heart/OlinkGenes_Heart.txt")
+head(myTab)
+
+#' # Functions for this script ####
+#' ***
+
+HierFDR_Holger = function(data){
+  # data = pQTLs_a
+  
+  data2 = copy(data)
+  
+  myFDR<- addHierarchFDR(pvalues = data2[,pval], categs = data2[,gene],quiet = T)
+  data2[,hierarch_fdr5proz:=myFDR$hierarch_fdr5proz]
+  
+  x1 = myFDR[,.(min_level1 = min(fdr_level1)), by = list(category,fdr_level2)]
+  x1[,hierarch_fdr5proz:=fdr_level2<0.05]
+  x0 = myFDR[hierarch_fdr5proz==T,.N,.(category,fdr_level2)]
+  matched<-match(x1$category,x0$category)
+  x1[,N:=x0$N[matched]]
+  x1[is.na(N),N:=0]
+  
+  return(x1)
+} 
+
+#' # Load Olink data hg38 ####
+#' ***
+pQTLs_a = fread("../exampleData_Heart/pQTLs_combined_hg38.txt.gz")
+pQTLs_f = fread("../exampleData_Heart/pQTLs_females_hg38.txt.gz")
+pQTLs_m = fread("../exampleData_Heart/pQTLs_males_hg38.txt.gz")
+
+pQTLs_a[,length(unique(gene))]
+pQTLs_f[,length(unique(gene))]
+pQTLs_m[,length(unique(gene))]
+
+#' # Hierarchical FDR ####
+#' ***
+#' 
+#' * Level 1: SNPs per gene
+#' * Level 2: lead SNPs per gene 
+#' * get k as number of genes associated with at least one SNP with $\alpha = 0.05$
+#' * define adjusted $\alpha_2$ as $\alpha_2 = \alpha \cdot k/n$ 
+#' * use $\alpha_2$ on Level 1 FDRs 
+#' 
+myFDR_a<- addHierarchFDR(pvalues = pQTLs_a[,pval], categs = pQTLs_a[,gene],quiet = F)
+myFDR_m<- addHierarchFDR(pvalues = pQTLs_m[,pval], categs = pQTLs_m[,gene],quiet = F)
+myFDR_f<- addHierarchFDR(pvalues = pQTLs_f[,pval], categs = pQTLs_f[,gene],quiet = F)
+
+pQTLs_a[,hierarch_fdr5proz:=myFDR_a$hierarch_fdr5proz]
+pQTLs_m[,hierarch_fdr5proz:=myFDR_m$hierarch_fdr5proz]
+pQTLs_f[,hierarch_fdr5proz:=myFDR_f$hierarch_fdr5proz]
+
+x1 = HierFDR_Holger(data = pQTLs_a)
+x2 = HierFDR_Holger(data = pQTLs_m)
+x3 = HierFDR_Holger(data = pQTLs_f)
+
+dim(x1[hierarch_fdr5proz==T])
+dim(x2[hierarch_fdr5proz==T])
+dim(x3[hierarch_fdr5proz==T])
+
+#' There are 59 significantly associated genes in the combined setting, 47 in males only, and 48 in females only.
+#' 
+#' Hence, I can define the BBFDR as FDR * (92/k) in the different settings 
+#' 
+pQTLs_a[,BBFDR:= FDR * 92/59]
+pQTLs_m[,BBFDR:= FDR * 92/47]
+pQTLs_f[,BBFDR:= FDR * 92/48]
+
+#' # Summary ####
+#' ***
+#' In x1, x2, and x3: 
+#' 
+#' * min_level1 is the Simes p-value. 
+#' * fdr_level2 is the FDR corrected Simes p-value -> pheno counted as significant if fdr_level2 < 0.05 x (k/n)
+#' * hierarch_fdr5proz is a T/F flag if pheno is significanlty associated with at least one SNP
+#' * N is the number of associated SNPs
+#'  
+myFDR_short = cbind(x1,x2[,-1],x3[,-1])
+names(myFDR_short)[2:13]<-c("FDRl2_a","SimesP_a","Sig_a","NSig_a",
+                            "FDRl2_m","SimesP_m","Sig_m","NSig_m",
+                            "FDRl2_f","SimesP_f","Sig_f","NSig_f")
+head(myFDR_short)
+dim(myFDR_short)
+
+table(myFDR_short$Sig_f,myFDR_short$Sig_m,myFDR_short$Sig_a)
+
+dummy<-myTab[,c(2,3,11)]  
+stopifnot(dummy$Gene == myFDR_short$category)
+myFDR2<-cbind(dummy,myFDR_short[,-1])
+
+myTab[,GWAS_sig:=myFDR_short$Sig_a]
+myTab[,SimesP:=myFDR_short$SimesP_a]
+myTab[,nSNPs:=myFDR_short$NSig_a]
+
+myTab[,GWAS_sig_male:=myFDR_short$Sig_m]
+myTab[,SimesP_male:=myFDR_short$SimesP_m]
+myTab[,nSNPs_male:=myFDR_short$NSig_m]
+
+myTab[,GWAS_sig_female:=myFDR_short$Sig_f]
+myTab[,SimesP_female:=myFDR_short$SimesP_f]
+myTab[,nSNPs_female:=myFDR_short$NSig_f]
+head(myTab)
+
+#' # Save ####
+#' ***
+save(myTab,file="../temp_Heart/02_OlinkGenes.RData")
+write.table(myFDR_short,file="../results_Heart/02_pQTLs_sigGenes.txt",
+            col.names = T,row.names = F, quote = F, sep="\t",dec = ",")
+
+save(pQTLs_a,file="../results_Heart/02_pQTLs_all_cis.RData")
+save(pQTLs_m,file="../results_Heart/02_pQTLs_males_cis.RData")
+save(pQTLs_f,file="../results_Heart/02_pQTLs_females_cis.RData")
+
+#' # SessionInfo
+#' ***
+sessionInfo()
+message("\nTOTAL TIME : " ,round(difftime(Sys.time(),time0,units = "mins"),3)," minutes")
---
+++ scripts_Heart/03_LeadSNPs_sexInteraction.R
@@ -0,0 +1,209 @@
+#' ---
+#' title: "Lead SNPs: sex interaction"
+#' subtitle: "LWAS Olink - Heart"
+#' author: "Janne Pott"
+#' date: "29. March 2022"
+#' output:
+#'   html_document:
+#'     toc: true
+#'     number_sections: true
+#'     toc_float: true
+#'     code_folding: show
+#' ---
+#'
+#' # Intro ####
+#' ***
+#' I want first to define the lead SNP per gene and setting and then to compare the effect estimates in males and females (are they sex-specific or sex-related?).
+#' 
+#' # Init ####
+#' ***
+rm(list = ls())
+time0<-Sys.time()
+
+source("../mySourceFile.R")
+.libPaths()
+setwd(projectpath_main_Heart)
+
+#' # Load & Prep Olink data ####
+#' ***
+#' Created by script 02_Hierarchical_FDR.R
+#' 
+load("../temp_Heart/02_OlinkGenes.RData")
+load("../results_Heart/02_pQTLs_all_cis.RData")
+load("../results_Heart/02_pQTLs_males_cis.RData")
+load("../results_Heart/02_pQTLs_females_cis.RData")
+
+#' # Get lead SNPs ####
+#' ***
+leadpQTLs1<-pQTLs_a[hierarch_fdr5proz==T,.SD[pval==min(pval)],by=.(gene)]
+leadpQTLs2<-pQTLs_m[hierarch_fdr5proz==T,.SD[pval==min(pval)],by=.(gene)]
+leadpQTLs3<-pQTLs_f[hierarch_fdr5proz==T,.SD[pval==min(pval)],by=.(gene)]
+
+table(duplicated(leadpQTLs1$gene))
+table(leadpQTLs1$gene) # LPL & AGER
+
+table(duplicated(leadpQTLs2$gene))
+table(duplicated(leadpQTLs3$gene))
+table(leadpQTLs3$gene) # LPL & IL17D
+
+leadpQTLs<-rbind(leadpQTLs1,leadpQTLs2,leadpQTLs3)
+leadpQTLs[,dumID:=paste(variant_id_hg38,gene,sep="__")]
+
+save(leadpQTLs, file="../results/03_leadSNPs.RData")
+
+#' # Look-up all lead SNPs in other settings ####
+#' ***
+mySNPs<-unique(leadpQTLs$variant_id_hg38)
+
+missingSNPs<-unique(c(mySNPs[!is.element(mySNPs,pQTLs_a[,variant_id_hg38])],
+                      mySNPs[!is.element(mySNPs,pQTLs_m[,variant_id_hg38])],
+                      mySNPs[!is.element(mySNPs,pQTLs_f[,variant_id_hg38])]))
+
+dummy = leadpQTLs[variant_id_hg38 %in% missingSNPs,]
+length(unique(dummy$gene))
+dummy
+
+#' Okay, all SNPs are available in all settings, I simply delete the second entry per gene.
+#' 
+leadpQTLs1 = leadpQTLs1[!duplicated(gene)]
+leadpQTLs3 = leadpQTLs3[!duplicated(gene)]
+leadpQTLs<-rbind(leadpQTLs1,leadpQTLs2,leadpQTLs3)
+leadpQTLs[,dumID:=paste(variant_id_hg38,gene,sep="__")]
+
+save(leadpQTLs, file="../results/03_leadSNPs.RData")
+
+leadpQTLs_a<-pQTLs_a[variant_id_hg38 %in% mySNPs,]
+leadpQTLs_a[,dumID:=paste(variant_id_hg38,gene,sep="__")]
+leadpQTLs_a<-leadpQTLs_a[dumID %in% leadpQTLs[,dumID]]
+
+leadpQTLs_m<-pQTLs_m[variant_id_hg38 %in% mySNPs,]
+leadpQTLs_m[,dumID:=paste(variant_id_hg38,gene,sep="__")]
+leadpQTLs_m<-leadpQTLs_m[dumID %in% leadpQTLs[,dumID]]
+
+leadpQTLs_f<-pQTLs_f[variant_id_hg38 %in% mySNPs,]
+leadpQTLs_f[,dumID:=paste(variant_id_hg38,gene,sep="__")]
+leadpQTLs_f<-leadpQTLs_f[dumID %in% leadpQTLs[,dumID]]
+
+table(leadpQTLs_a$variant_id_hg38==leadpQTLs_f$variant_id_hg38)
+table(leadpQTLs_a$variant_id_hg38==leadpQTLs_m$variant_id_hg38)
+
+#' # Combine settings ####
+#' ***
+names(leadpQTLs_a)
+myFDR4<-cbind(leadpQTLs_a[,c(24,19,18,2,1,12,7,9,13:15,17,11,22)],
+              leadpQTLs_m[,c(7,9,13:15,17,11,22)],
+              leadpQTLs_f[,c(7,9,13:15,17,11,22)])
+names(myFDR4)
+names(myFDR4)<-c("dumID","gene","cyto","variant_id_hg38","variant_id_hg19","info",
+                 "ea_a","eaf_a","beta_a","se_a","pval_a","zscore_a","n_a","hfdr_a",
+                 "ea_m","eaf_m","beta_m","se_m","pval_m","zscore_m","n_m","hfdr_m",
+                 "ea_f","eaf_f","beta_f","se_f","pval_f","zscore_f","n_f","hfdr_f")
+myFDR4
+
+plot(myFDR4$eaf_a,myFDR4$eaf_m)
+plot(myFDR4$eaf_a,myFDR4$eaf_f)
+
+table(myFDR4$ea_a==myFDR4$ea_m)
+table(myFDR4$ea_a==myFDR4$ea_f)
+
+#' # Interaction test ####
+#' ***
+interactionTest_classic  = function(mean1, se1, mean2, se2) { 
+  ## interaction test see http://www.bmj.com/content/326/7382/219.long
+  meandiff_se = sqrt(se1^2 + se2^2)
+  meandiff = mean2 - mean1
+  meandiff_cilow = meandiff - 1.96*meandiff_se
+  meandiff_cihigh = meandiff + 1.96*meandiff_se
+  meandiff_z = meandiff/meandiff_se
+  meandiff_p = pnorm(abs(meandiff_z), lower.tail = F)*2
+  if(meandiff_p>1) meandiff_p = 1
+  data.table::data.table(mean1, se1, mean2, se2, meandiff, meandiff_se, meandiff_cilow, meandiff_cihigh, meandiff_z, meandiff_p)
+}
+
+sexIA<-interactionTest_classic(myFDR4$beta_m,myFDR4$se_m,myFDR4$beta_f,myFDR4$se_f)
+table(sexIA$meandiff_p<0.05)
+sexIA[,meandiff_p_FDR:=p.adjust(meandiff_p,method = "BH")]
+sexIA[,table(meandiff_p<0.05,meandiff_p_FDR<0.05)]
+
+myFDR5<-cbind(myFDR4,sexIA[,c(5,6,10,11)])
+myFDR5[meandiff_p_FDR<0.05]
+myFDR5[meandiff_p<0.05]
+myFDR5[meandiff_p>=0.05,sexIA_type:="sex-unspecific"]
+myFDR5[meandiff_p<0.05 & hfdr_m==T & hfdr_f==F,sexIA_type:="male-specific"]
+myFDR5[meandiff_p<0.05 & hfdr_m==F & hfdr_f==T,sexIA_type:="female-specific"]
+myFDR5[meandiff_p<0.05 & hfdr_m==T & hfdr_f==T,sexIA_type:="sex-related"]
+
+#' # Plotting ####
+#' ***
+#' I want a beta-beta plot. None-significant interaction should be black, nominal-significant interaction should be colored, and FDR-significant interaction should be labeled.
+#' 
+ 
+myPlotData<-data.table(dumID=myFDR5$dumID,
+                       rs_id=myFDR5$variant_id_hg38,
+                       cyto=myFDR5$cyto,
+                       gene=myFDR5$gene,
+                       eaf_male = myFDR5$eaf_m,
+                       beta_male = myFDR5$beta_m,
+                       eaf_female = myFDR5$eaf_f,
+                       beta_female = myFDR5$beta_f,
+                       meandiff = myFDR5$meandiff,
+                       meandiff_p = myFDR5$meandiff_p,
+                       meandiff_p_FDR = myFDR5$meandiff_p_FDR,
+                       sexIA_type = myFDR5$sexIA_type)
+myPlotData[meandiff_p>=0.05,sig:="no"]
+myPlotData[meandiff_p<0.05,sig:="yes"]
+myPlotData[meandiff_p_FDR<0.05,sig:="yes (FDR 5%)"]
+myPlotData[,gene2:=""]
+myPlotData[meandiff_p_FDR<0.05,gene2:=gene]
+myPlotData[,gene3:=""]
+myPlotData[meandiff_p<0.05,gene3:=gene]
+
+myPlot = ggplot(myPlotData[sig!="no",], aes(x=beta_male, y=beta_female, color=gene3,label=gene2,shape = sexIA_type)) +
+  geom_abline(intercept = 0, slope = 1, color="grey", linetype="dashed", size=1.25)+
+  geom_hline(yintercept = 0, color="grey", linetype="dashed", size=1.15)+
+  geom_vline(xintercept = 0, color="grey", linetype="dashed", size=1.15)+
+  geom_point(data=myPlotData[sig=="no",], aes(x=beta_male, y=beta_female),col="black",size=1.5,alpha=0.5,shape = 16)+
+  geom_point(size=3.5)+ 
+  theme_bw(base_size = 10)+
+  theme(plot.title = element_text(hjust = 0, size=22,face="bold"),
+        axis.title.x = element_text(size=12,face="bold"),
+        axis.title.y = element_text(size=12,face="bold"),
+        axis.text = element_text(size=12,face="bold"))+
+  labs(x="Effect in males", y = "Effect in females",
+       color = "Genes with \nsex interaction", shape = "Type of \nsex interaction")+
+  guides(size="none",fill="none")+
+  scale_shape_manual(values=c(15, 17, 18,16))+
+  geom_label_repel(data = subset(myPlotData, sig!="no"),# & type=="beta estimate"),
+                   aes(fill=gene3),
+                   box.padding = 1.15,
+                   max.overlaps = 25,
+                   show.legend=F,color="black")
+myPlot
+
+tiff(filename = "../results/03_ScatterPlot_sexIA.tif", 
+     width = 1200, height = 900, res=125, compression = 'lzw')
+myPlot
+dev.off()
+
+#' # Save ####
+#' ***
+leadSNPs_IA<-copy(myFDR5)
+save(leadSNPs_IA,file="../results/03_leadSNPs_IA.RData")
+
+sigLoci_IA_sig<-copy(myFDR5)
+sigLoci_IA_sig = sigLoci_IA_sig[meandiff_p<0.05,]
+
+myTab[,sexIA:=F]
+myTab[Gene %in% sigLoci_IA_sig$gene,sexIA:=T]
+head(myTab)
+matched = match(myTab$Gene,sigLoci_IA_sig$gene)
+table(is.na(matched))
+length(unique(sigLoci_IA_sig$gene))
+myTab[,sexIA_type:=sigLoci_IA_sig$sexIA_type[matched]]
+
+save(myTab,file="../temp/03_OlinkGenes.RData")
+
+#' # sessioninfo ####
+#' ***
+sessionInfo()
+message("\nTOTAL TIME : " ,round(difftime(Sys.time(),time0,units = "mins"),3)," minutes")
